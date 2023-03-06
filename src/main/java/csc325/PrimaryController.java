821ee9623ad8933cc5adfffad823e2064ba2563b
Vasqjr <93558391+Vasqjr@users.noreply.github.com>
2023-03-05 19:40:29 -0500

    Login Button now Disables Upon Press. (Possibly Optimizable)

--- src/main/java/csc325/PrimaryController.java
+++ src/main/java/csc325/PrimaryController.java
@@ -19,22 +19,33 @@ public class PrimaryController {
     private Button enterButton;
     
     @FXML
-    private void handleLoginButton(ActionEvent event) throws IOException {
+    private void handleLoginButton() throws IOException {
     // code to verify login credentials and switch to main page
     
-   KeyValue disableValue1 = new KeyValue(enterButton.disableProperty(), false);
-   KeyFrame disableFrame1 = new KeyFrame(Duration.INDEFINITE, disableValue1);
-   KeyValue disableValue2 = new KeyValue(enterButton.disableProperty(), true);
-   KeyFrame disableFrame2 = new KeyFrame(Duration.ZERO, disableValue2);
-   Timeline disableTimeLine = new Timeline(disableFrame2, disableFrame1);
-   disableTimeLine.play();
+        KeyValue disableValue1 = new KeyValue(enterButton.disableProperty(), false);
+        KeyFrame disableFrame1 = new KeyFrame(Duration.INDEFINITE, disableValue1);
+        KeyValue disableValue2 = new KeyValue(enterButton.disableProperty(), true);
+        KeyFrame disableFrame2 = new KeyFrame(Duration.ZERO, disableValue2);
+        Timeline disableTimeLine = new Timeline(disableFrame2, disableFrame1);
+        disableTimeLine.play();
     
-    Stage appStage = new Stage();
-    Scene appScene = new Scene(FXMLLoader.load(getClass().getResource("primary.fxml")));
+        Stage appStage = new Stage();
+        Scene appScene = new Scene(FXMLLoader.load(getClass().getResource("primary.fxml")));
     
-    appStage.setScene(appScene);
-    appStage.setWidth(1280); // set the initial width of the main page's window
-    appStage.setHeight(720); // set the initial height of the main page's window
-    appStage.show();
-}
+        appStage.setScene(appScene);
+        appStage.setWidth(1280); // set the initial width of the main page's window
+        appStage.setHeight(720); // set the initial height of the main page's window
+        appStage.show();
+    }
+    
+    @FXML
+    private void handleCreateAccountPageOpenLabel() throws IOException{
+        Stage createStage = new Stage();
+        Scene createScene = new Scene(FXMLLoader.load(getClass().getResource("createAccount.fxml")));
+    
+        createStage.setScene(createScene);
+        createStage.setWidth(350); // set the initial width of the main page's window
+        createStage.setHeight(450); // set the initial height of the main page's window
+        createStage.show();
+    }
 }
