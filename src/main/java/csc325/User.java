aa6bae787fdcf3f7202cf6928ee28aaac7cf9909
NRShah25 <103145108+NRShah25@users.noreply.github.com>
2023-03-05 19:13:41 -0500

    Update User.java
    Created basic member variables needed for a user

--- src/main/java/csc325/User.java
+++ src/main/java/csc325/User.java
@@ -9,5 +9,45 @@ package csc325;
  * @author nicholasshah
  */
 public class User {
+    private String firstName;
+    private String userName;
+    private String userPassword;
+    private String homeZipCode;
+    private String [] recentLocations;
+
+    public String getHomeZipCode() {
+        return homeZipCode;
+    }
+
+    public void setHomeZipCode(String homeZipCode) {
+        this.homeZipCode = homeZipCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
     
+    public void createUser(){
+        
+    }
 }
