3f1e681c42d1093b6836a6daad47d89c2882450c
NRShah25 <103145108+NRShah25@users.noreply.github.com>
2023-03-05 20:16:59 -0500

    Update PrimaryController.java

--- src/main/java/csc325/PrimaryController.java
+++ src/main/java/csc325/PrimaryController.java
@@ -15,42 +15,46 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class PrimaryController {
+
     @FXML
     private TextField firstNameText;
-     @FXML
+    @FXML
     private TextField userNameText;
-      @FXML
+    @FXML
     private TextField passwordText;
-       @FXML
+    @FXML
     private TextField homeZipCodeText;
     @FXML
     private Button enterButton;
-    
+
     @FXML
     private void handleLoginButton() throws IOException {
-    // code to verify login credentials and switch to main page
-    
+        // code to verify login credentials and switch to main page
+
         Stage stage = (Stage) enterButton.getScene().getWindow();
         stage.close();
-    
+
         Stage appStage = new Stage();
         Scene appScene = new Scene(FXMLLoader.load(getClass().getResource("primary.fxml")));
-    
+
         appStage.setScene(appScene);
         appStage.setWidth(1280); // set the initial width of the main page's window
         appStage.setHeight(720); // set the initial height of the main page's window
         appStage.show();
-        
+
     }
-    
+
     @FXML
-    private void handleCreateAccountPageOpenLabel() throws IOException{
+    private void handleCreateAccountPageOpenLabel() throws IOException {
         Stage createStage = new Stage();
         Scene createScene = new Scene(FXMLLoader.load(getClass().getResource("createAccount.fxml")));
-    
+
         createStage.setScene(createScene);
         createStage.setWidth(350); // set the initial width of the main page's window
         createStage.setHeight(450); // set the initial height of the main page's window
         createStage.show();
+    }
+
+    public void handleCreateAccount() {
     }
 }
