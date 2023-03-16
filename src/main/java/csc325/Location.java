6c29ac862feaf75311c1ffd70e2ee7c92798e278
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-03-16 14:46:36 -0400

    Update Location.java

--- src/main/java/csc325/Location.java
+++ src/main/java/csc325/Location.java
@@ -5,5 +5,12 @@ package csc325;
  * @author Jonathan Crain
  */
 public class Location {
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    
+    
     
 }
