fce875d85fe20a37ec47544f8565b8fa06e7a262
Vasqjr <93558391+Vasqjr@users.noreply.github.com>
2023-03-24 15:20:15 -0400

    Update Home.java

--- src/main/java/csc325/Home.java
+++ src/main/java/csc325/Home.java
@@ -1,17 +1,52 @@
 package csc325;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 public class Home {
 
     @FXML
-    private void displayChartData() throws IOException {
-        XYChart.Series<String, Number> weatherDisplay = new XYChart.Series<>();
+    private LineChart<String, Number> homeForecastLineChart;
+    
+    @FXML
+    private NumberAxis homeNumberAxis;
+    
+    @FXML
+    private CategoryAxis homeCategoryAxis;
+    
+    public void initialize() throws IOException{
+        displayChartData();
+    }
+    
+    public void displayChartData() throws IOException {
+        // create a number axis for the y-axis
         DetailedWeather d1 = new DetailedWeather();
-        d1.getDetailedWeather();
-        
-        //weatherDisplay.getData().add(new XYChart.Data<>(d1.getDetailedWeather().))
+        XYChart.Series<String, Number> weather = new XYChart.Series<>();
+
+        // add some data points to the series
+        weather.getData().add(new XYChart.Data<>("Monday", 23));
+        weather.getData().add(new XYChart.Data<>("Tuesday", 22));
+        weather.getData().add(new XYChart.Data<>("Wednesday", 24));
+        weather.getData().add(new XYChart.Data<>("Thursday", 23));
+        weather.getData().add(new XYChart.Data<>("Friday", 21));
+        weather.getData().add(new XYChart.Data<>("Saturday", 19));
+        weather.getData().add(new XYChart.Data<>("Sunday", 18));
+
+        // add the data series to the chart
+        homeForecastLineChart.getData().add(weather);
     }
 }
