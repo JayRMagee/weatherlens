4291bf6cb02c01c80d49e9271518868e227bee8f
Vasqjr <93558391+Vasqjr@users.noreply.github.com>
2023-03-31 13:15:37 -0400

    Update Home.java

--- src/main/java/csc325/Home.java
+++ src/main/java/csc325/Home.java
@@ -12,12 +12,15 @@ import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class Home {
 
@@ -43,11 +46,23 @@ public class Home {
         for (int i = 1; i <= 13; i += 2) {
             String day = d1.getDay(i);
             int temperature = d1.getTemperature(i);
+            String iconLink = d1.getIcon(i);
             
             XYChart.Data<String, Number> data = new XYChart.Data<>(day, temperature);
-            weather.getData().add(new XYChart.Data<>(day, temperature));
-            
             weather.getData().add(data);
+            
+            // create an image view for the icon and add it to the data point
+            Image icon = new Image(iconLink);
+            ImageView imageView = new ImageView(icon);
+            imageView.setFitWidth(40);
+            imageView.setFitHeight(40);
+            imageView.setClip(new Circle(20,20,20));
+            Circle circle = new Circle(20, 20, 22);
+            circle.setStroke(Color.BLACK);
+            circle.setStrokeWidth(2);
+        
+            Group group = new Group(circle, imageView);
+            data.setNode(group);
         }
 
         // add the data series to the chart
