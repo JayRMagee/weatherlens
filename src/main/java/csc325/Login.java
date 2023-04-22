6877c8d7b326bb2d35e8e531cd40577c9b6b8f52
Vasqjr <93558391+Vasqjr@users.noreply.github.com>
2023-04-22 19:26:01 -0400

    Update Login.java

--- src/main/java/csc325/Login.java
+++ src/main/java/csc325/Login.java
@@ -61,9 +61,8 @@ public class Login {
         Scene appScene = new Scene(FXMLLoader.load(getClass().getResource("home.fxml")));
 
         appStage.setScene(appScene);
-        appStage.setWidth(1200);
-        appStage.setHeight(830);
-        appStage.sizeToScene();
+        appStage.setWidth(1220);
+        appStage.setHeight(825);
         appStage.setResizable(false);
         appStage.show();
 
