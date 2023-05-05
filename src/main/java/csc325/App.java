7bc425b633144cd09a8fb373ce310e97eec40496
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-05-05 18:39:52 -0400

    Added Firebase SDK.
    Added Firebase dependencies to pom.xml and created inner class FirebaseContext within App.java.

--- src/main/java/csc325/App.java
+++ src/main/java/csc325/App.java
@@ -1,5 +1,9 @@
 package csc325;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,11 +12,25 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import static javafx.application.Application.launch;
-
+import com.google.firebase.cloud.FirestoreClient;
 /**
  * JavaFX App
  */
 public class App extends Application {
+    
+    public class FirestoreContext {
+        public Firestore firebase() {
+            try {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(getClass().getResourceAsStream("csc325weatherlens-firebase-adminsdk-d1394-0025d4fab8.json")))
+                    .build();
+                FirebaseApp.initializeApp(options);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return FirestoreClient.getFirestore();
+        }
+    }
 
     private static Scene scene;
     public static Stage stage = null;                                           // placeholder stage to hold the original version of the login page.
